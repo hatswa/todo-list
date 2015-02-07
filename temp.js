648b319ba6c48d24ad6f5d55c4ff36bcbfcf4f06
zhangoose <hi.zhalex@gmail.com>
2015-02-06 19:34:15 -0500

    TEMP

---
+++ temp.js
@@ -0,0 +1,21 @@
+var count = 23;
+var nothing = null;
+var str = "hello";
+var ch = 'c';
+var array = [1,2,3,3]; 
+
+function fun(one, two){
+	//blah
+}
+
+var fun = function(one, two){
+	// blah
+};
+
+/*
+	 <script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
+	 <script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+*/
+
+
+
